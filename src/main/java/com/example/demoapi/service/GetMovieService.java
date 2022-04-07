78e03dd294e0745018e7445102c6ddfb62d42c4b
TheKeiseur <arthur.sicard@gmail.com>
2022-04-07 13:37:29 +0200

    refactored into services

--- src/main/java/com/example/demoapi/service/GetMovieService.java
+++ src/main/java/com/example/demoapi/service/GetMovieService.java
@@ -5,13 +5,19 @@ import com.example.demoapi.repository.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class MovieService {
+public class GetMovieService {
 
     @Autowired
     private MovieRepository movieRepository;
 
     public Movie getMovie(Long id) {
         return movieRepository.findById(id).get();
+    }
+
+    public List<Movie> getMovies() {
+        return movieRepository.findAll();
     }
 }
---
+++ src/main/java/com/example/demoapi/service/SaveMovieService.java
@@ -0,0 +1,30 @@
+package com.example.demoapi.service;
+
+import com.example.demoapi.model.Movie;
+import com.example.demoapi.repository.MovieRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class SaveMovieService {
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    public Boolean saveMovie(String title, Integer duration, String description) {
+        Movie movie = new Movie();
+        movie.setTitle(title);
+        movie.setDuration(duration);
+        movie.setDescription(description);
+        try {
+            movieRepository.save(movie);
+            return true;
+        } catch (Exception e) {
+            log.error(e.getLocalizedMessage());
+            return true;
+        }
+
+    }
+}
